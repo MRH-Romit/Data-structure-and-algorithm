294888e8e2dccb461fc057c047a3c526f569cb29
MRH-Romit <mdromit2002@gmail.com>
2024-07-13 22:49:00 +0600

    refactor: Improve QuickSort implementation in C++

--- sorting techniques/QuickSort.cpp
+++ sorting techniques/QuickSort.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
-int n,pivot,i,swap,j;
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-void pivot
+void quicksort(int arr[], int first, int last) {
+    if (first < last) {
+        int pivot = first;
+        int i = first;
+        int j = last;
+        while (i < j) {
+            while (arr[i] <= arr[pivot] && i < last) {
+                i++;
+            }
+            while (arr[j] > arr[pivot]) {
+                j--;
+            }
+            if (i < j) {
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[pivot], arr[j]);
+        quicksort(arr, first, j - 1);
+        quicksort(arr, j + 1, last);
+    }
+}
 
 
 
@@ -11,5 +36,6 @@ void pivot
 
 
 
-// worst case time complexity of quick sort is O(n^2) if it is sorted already 
-// best case time complexity is O(nlogn)
+
+// worst case time complexity of quick sort is O(n^2) if the pivot element is the smallest or largest element in the array
+// best case time complexity is O(nlogn) if the pivot element is the median element in the array 
