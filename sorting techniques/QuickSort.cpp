6691b9afad87bdd523b21c36cb9b80820800e649
MRH-Romit <mdromit2002@gmail.com>
2024-07-13 22:37:52 +0600

    chore: Add QuickSort implementation in C++

---
+++ sorting techniques/QuickSort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int n,pivot,i,swap,j;
+
+void pivot
+
+
+
+
+
+
+
+// worst case time complexity of quick sort is O(n^2) if it is sorted already 
+// best case time complexity is O(nlogn)
