9c91387dcbd2b3e8b2df1c8a0eebd998403900dc
MRH-Romit <mdromit2002@gmail.com>
2024-11-20 20:08:24 +0600

    feat: Implement Merge Sort algorithm in C++

--- merge Sort/practice.cpp
+++ merge Sort/practice.cpp
@@ -1,2 +1,46 @@
 // time complextity of merge sort is O(nlogn)
 // space complexity of merge sort is O(n)
+// merge sort is a stable sort
+
+#include <iostream>
+
+using namespace std;
+
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    
+    int L[n1], R[n2];
+    
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        R[i] = arr[m + 1 + i];
+    }
+    
+    int i = 0, j = 0, k = l;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
